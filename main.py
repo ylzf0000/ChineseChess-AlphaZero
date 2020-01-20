44427e5de57dc300f1c68b1702f9286c5a6e1f1e
Zhang <84978695@qq.com>
2020-01-20 12:19:15 +0800

    。

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # coding=utf-8
 import os
+import sys
 import agent
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from mainwindow import *
 
-# os.environ["CUDA_VISIBLE_DEVICES"] = "2,3"
+sys.setrecursionlimit(1000000)
+os.environ["CUDA_VISIBLE_DEVICES"] = "2,3"
 if __name__ == '__main__':
     agent.train()
     # app = QApplication(sys.argv)
